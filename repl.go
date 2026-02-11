0e0e70e78d4228ec81338b57d5aa40038d0aa341
Heros-Tempus <Heros_Tempus@yahoo.com>
2026-02-10 19:33:35 -0500

    Implement REPL functionality with command handling for help and exit

--- repl.go
+++ repl.go
@@ -1,9 +1,55 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := cleanInput(scanner.Text())[0]
+		command, exists := getCommands()[input]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
 
 func cleanInput(text string) []string {
-	//takes a string as input and returns a slice of lowercase strings split on whitespace
 	lowercase := strings.ToLower(text)
 	return strings.Fields(lowercase)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	var commands map[string]cliCommand = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Display help information",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+	return commands
 }
